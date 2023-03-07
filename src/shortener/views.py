c19f4702a4b58ad50b34aeb7a84810ebe5b5d516
maiquegouveia <maiquebusiness@gmail.com>
2023-03-07 16:43:05 -0300

    Created HomeView and implemented redirect for KirrCBView

--- src/shortener/views.py
+++ src/shortener/views.py
@@ -1,12 +1,35 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
 from django.views import View
 
+from .forms import SubmitUrlForm
+from .models import KirrURL
 
 # Create your views here.
-def kirr_redirect_view(request, *args, **kwargs):
-    return HttpResponse('hello')
 
-class KirrCBView(View):
+class HomeView(View):
     def get(self, request, *args, **kwargs):
-        return HttpResponse('hello again')
+        the_form = SubmitUrlForm()
+        context = {
+            'title': 'URL.co',
+            'form': the_form,
+        }
+        return render(request, 'shortener/home.html', context)
+    
+    def post(self, request, *args, **kwargs):
+        form = SubmitUrlForm(request.POST)
+
+        if form.is_valid():
+            print(form.cleaned_data.get('url'))
+        
+        context = {
+            'title': 'URL.co',
+            'form': form,
+        }
+        return render(request, 'shortener/home.html', context)
+    
+    
+class KirrCBView(View):
+    def get(self, request, shortcode=None, *args, **kwargs):
+        obj = get_object_or_404(KirrURL, shortcode=shortcode)
+        return HttpResponseRedirect(obj.url)
