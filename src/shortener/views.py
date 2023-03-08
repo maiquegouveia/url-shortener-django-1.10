cee8475bbcc0bd50ac6fb63e5d6fa1647169cbd1
maiquegouveia <maiquebusiness@gmail.com>
2023-03-07 23:59:59 -0300

    Updated views

--- src/shortener/views.py
+++ src/shortener/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.views import View
 
+from analytics.models import ClickEvent
+
 from .forms import SubmitUrlForm
 from .models import KirrURL
 
@@ -18,18 +20,28 @@ class HomeView(View):
     
     def post(self, request, *args, **kwargs):
         form = SubmitUrlForm(request.POST)
-
-        if form.is_valid():
-            print(form.cleaned_data.get('url'))
-        
         context = {
             'title': 'URL.co',
             'form': form,
         }
-        return render(request, 'shortener/home.html', context)
+        template = 'shortener/home.html'
+        if form.is_valid():
+            new_url = form.cleaned_data.get('url')
+            obj, created = KirrURL.objects.get_or_create(url=new_url)
+            context = {
+                'object': obj,
+                'created': created,
+            }
+            if created:
+                template = 'shortener/success.html'
+            else:
+                template = 'shortener/already-exists.html'
+            
+        return render(request, template, context)
     
     
-class KirrCBView(View):
+class URLRedirectView(View):
     def get(self, request, shortcode=None, *args, **kwargs):
         obj = get_object_or_404(KirrURL, shortcode=shortcode)
+        print(ClickEvent.objects.create_event(obj))
         return HttpResponseRedirect(obj.url)
