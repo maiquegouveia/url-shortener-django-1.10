7dcfcc79d2340273fa861323b90c8395eda6d0fb
maiquegouveia <maiquebusiness@gmail.com>
2023-03-08 00:00:35 -0300

    Added validators and widget to forms

--- src/shortener/forms.py
+++ src/shortener/forms.py
@@ -4,7 +4,13 @@ from .validators import validate_dot_com, validate_url
 
 
 class SubmitUrlForm(forms.Form):
-    url = forms.CharField(label='Submit Url', validators=[
-            validate_url,
-            validate_dot_com,
-            ])
+    url = forms.CharField(
+            label='',
+            validators=[validate_url],
+            widget=forms.TextInput(
+                attrs={
+                    'placeholder': 'Long URL',
+                    'class': 'form-control'
+                }
+            )
+            )
